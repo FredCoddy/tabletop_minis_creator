e26da15881a5b4153d553e96be1b22b77e03e07e
coddy33 <nanouwow@hotmail.fr>
2018-05-29 04:40:19 +0200

    update ancien js

--- html/js/menu_jquery.js
+++ html/js/menu_jquery.js
@@ -8,17 +8,45 @@
 
 console.log("script Jquery : OK")
 
-var scale = 0.7;
+// var file_name = '<?php $_FILES['fichier']['name'] ?>';
 
+// $("#get_filename").click(function(){
+//     console.log("coucou");
+//     var file_name = '<?php echo $file_name; ?>'; 
+//     console.log(file_name);
+// });
 
 /* Menu jQuery */
+
+$(window).on('wheel', function(e){
+    /** 
+     * Bind wheel mouse events for zoom and dezoom
+     * Need to specify the max and min scale 
+     *  
+     * Increase or decrease the $scale of the object by $step
+     * 
+     * Need zoom()
+     */
+    var step = 0.05;
+    var max_scale = 1.6;
+    var min_scale = 0.5;
+    var delta = e.originalEvent.deltaY;
+    if(delta > 0 && scale > 0.5){ // Down
+        scale-=step;
+        zoom(scale);
+    }
+    else if(scale < max_scale){ // UP
+        scale+=step;
+        zoom(scale);
+    }
+})
 
 $("#shark_button").click(function() {
     /**
      * Bind shark_button. Load the shark JSON object
      */
     remove_object();
-    load_JSON('json_object/shark2.json');
+    load_JSON('json_object/shark2.json', red);
     console.log("shark");
 });
 
@@ -44,29 +72,41 @@ $(".pause").click(function() {
     $("#animation").addClass("start_animation").removeClass("pause");
 });
 
-
-$(window).on('wheel', function(e){
-    /** 
-     * Bind wheel mouse events for zoom and dezoom
-     * Need to specify the max and min scale 
-     *  
-     * Increase or decrease the $scale of the object by $step
+$(".color_button").click(function(){
+    /**
      * 
-     * Need zoom()
+     * Get the "color_button" id ans set this color as material.
+     * 
+     * Need to set the color name as element id in html script AND 
+     * set the element class by "color_button"
+     * 
      */
-    var step = 0.05;
-    var max_scale = 1.6;
-    var min_scale = 0.5;
-    var delta = e.originalEvent.deltaY;
-    if(delta > 0 && scale > 0.5){ // Down
-        scale-=step;
-        zoom(scale);
-    }
-    else if(scale < max_scale){ // UP
-        scale+=step;
-        zoom(scale);
-    }
-})
+    // var name = $("#test").attr
+    color=$(this).attr("id");
+    def_color(color);
+    load_JSON('json_object/shark2.json')
+});
+
+
+/**
+ * 
+ * @param {*} col : color name
+ */
+function def_color(col){
+    
+    coloR=col;
+}
+
+//COLOR NAME
+var blue = 0x5A69ED;
+var red = 0xF00019;
+var green = 0x13E000;
+var grey = 0x77767F;
+
+var coloR = grey;
+
+
+var scale = 0.7;
 
 
 
@@ -79,12 +119,12 @@ $(window).on('wheel', function(e){
         var canvas = document.getElementById('canvas'); 
 
         var renderer = new THREE.WebGLRenderer({canvas: canvas});
-        renderer.setClearColor(0x577c93);
+        renderer.setClearColor(0xFFFFFF);
         renderer.setPixelRatio(window.devicePixelRatio);
-        // canvas.width  = canvas.clientWidth;
-        // canvas.height = canvas.clientHeight;
-        // renderer.setViewport(0, 0, canvas.clientWidth, canvas.clientHeight);
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        canvas.width  = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+        renderer.setViewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+        // renderer.setSize(window.innerWidth, window.innerHeight);
 
         //CAMERA
         var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
@@ -104,6 +144,9 @@ $(window).on('wheel', function(e){
 
         //MODEL 
     function load_JSON(objJSON){
+        if (mesh){
+            scene.remove(mesh);
+        }
         var loader = new THREE.JSONLoader();
         loader.load(objJSON, handle_load);
     }
@@ -114,24 +157,17 @@ $(window).on('wheel', function(e){
         //CONTROLS TODO
         // controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-   function handle_load(geometry, materials) {
-
-        //ANIMATION MESH
-        var material = new THREE.MeshLambertMaterial({ morphTargets: true });
-        
+    function handle_load(geometry, materials) {
+        // CHANGE MATERIAL COLOR | PhongMaterial = aspect brillant
+        var material = new THREE.MeshPhongMaterial({color:coloR});      
         mesh = new THREE.Mesh(geometry, material);
         scene.add(mesh);
         mesh.position.z = -10;
         mesh.position.y = -2;
         mesh.scale.set(scale,scale,scale);
 
-   
-        //MIXER
-        mixer = new THREE.AnimationMixer(mesh);
-
-        var clip = THREE.AnimationClip.CreateFromMorphTargetSequence('talk', geometry.morphTargets, 30);
-        mixer.clipAction(clip).setDuration(1).play();
     }
+
 
     function zoom(scale){
         mesh.scale.set(scale,scale,scale);
